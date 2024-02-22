3fb4347163e6214f87cbe642818c9888a583ce8d
Jose <jfernandeztamboleo@gmail.com>
2024-02-23 00:32:32 +0100

    add get by id, patch, delete to api functionality

--- 4-natours/app.js
+++ 4-natours/app.js
@@ -15,15 +15,77 @@ app.get('/api/v1/tours', (req, res) => {
   })
 })
 
+app.get('/api/v1/tours/:id', (req, res) => {
+  const { id } = req.params
+  const tourIndex = tours.findIndex(tour => tour.id.toString() === id)
+  if (tourIndex === -1) {
+    return res.status(404).json({ status: 'failed', message: 'invalid id' })
+  }
+  res.status(200).json({
+    status: 'success',
+    data: {
+      tour: tours[tourIndex]
+    }
+  })
+})
+
 app.post('/api/v1/tours', (req, res) => {
   const newId = tours[tours.length - 1].id + 1
   const newTour = Object.assign({ id: newId }, req.body)
   tours.push(newTour)
-  fs.writeFile(`${__dirname}/dev-data/data/tours-simple.json`, JSON.stringify(tours), (err, data) => {
-    if (err) throw new Error('error writing new tour')
+  fs.writeFile(`${__dirname}/dev-data/data/tours-simple.json`, JSON.stringify(tours), err => {
+    if (err) {
+      return res.status(500).json({ status: 'failed', message: 'error creating tour' })
+    }
     res.status(201).json({
       status: 'success',
-      tour: newTour
+      data: {
+        tour: newTour
+      }
+    })
+  })
+})
+
+app.patch('/api/v1/tours/:id', (req, res) => {
+  const { id } = req.params
+  console.log(req.body)
+  const tourIndex = tours.findIndex(tour => tour.id.toString() === id)
+  if (tourIndex === -1) {
+    return res.status(404).json({ status: 'failed', message: 'invalid id' })
+  }
+
+  const updatedTour = {
+    ...tours[tourIndex],
+    ...req.body
+  }
+  tours[tourIndex] = updatedTour
+
+  fs.writeFile(`${__dirname}/dev-data/data/tours-simple.json`, JSON.stringify(tours), err => {
+    if (err) res.status(500).json({ status: 'failed', message: 'error updating tour' })
+    res.status(200).json({
+      status: 'success',
+      data: {
+        tour: updatedTour
+      }
+    })
+  })
+})
+
+app.delete('/api/v1/tours/:id', (req, res) => {
+  const { id } = req.params
+  const newTours = tours.filter(tour => tour.id.toString() !== id)
+  if (tours.length === newTours.length) {
+    return res.json({ status: 'failed', message: 'tour not found' })
+  }
+
+  fs.writeFile(`${__dirname}/dev-data/data/tours-simple.json`, JSON.stringify(newTours), err => {
+    if (err) {
+      return res.status(500).json({ status: 'failed', message: 'error deleting tour' })
+    }
+    // res.status(204)
+    res.status(200).json({
+      status: 'success',
+      data: null
     })
   })
 })
